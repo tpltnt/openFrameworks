82f0b965060f01355b3d00aaadc1cefe7c0c30f7
tpltnt <tpltnt.github@dropcut.net>
2015-04-11 17:53:52 +0200

    added incomplete 'install_dependencies.sh

---
+++ scripts/bsd/freebsd/install_dependencies.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+set -e
+
+if [ $(id -u) != 0 ]; then
+	echo "this script must be run as root"
+	echo ""
+	echo "usage:"
+	echo "su -"
+	echo $0
+    exit 1
+fi
+
+# update local package cache
+pkg update
+
+echo "installing gstreamer"
+# install gstreamer 1.0
+# * gstreamer-ffmpeg not yet at 1.0
+# * FreeBSD has no ALSA, only OSS, therefore no gstreamer${GSTREAMER_VERSION}-alsa
+pkg install gstreamer1 gstreamer1-libav
+gstreamer-ffmpeg \
+gstreamer1-plugins-pulse \
+# base (partial?), good, bad, ugly
+gstreamer1-plugins
+# gstreamer plugins base not explixit ports, but
+# gstreamer1-plugins-cdparanoia, gstreamer-plugins-gio,
+# gstreamer1-plugins-libvisual, gstreamer1-plugins-ogg
+# gstreamer1-plugins-pango, gstreamer1-plugins-theora
+# gstreamer1-plugins-v4l2, gstreamer1-plugins-vorbis
+# gstreamer1-plugins-ximagesrc
+# complete list: http://gstreamer.freedesktop.org/data/doc/gstreamer/head/gst-plugins-base-plugins/html/
+exit_code=$?
+if [ $exit_code != 0 ]; then
+	echo "error installing gstreamer, there could be an error with your internet connection"
+    echo "if the error persists, please report an issue in github: http://github.com/openframeworks/openFrameworks/issues"
+	exit $exit_code
+fi
+
+
+echo "installing GTK 3"
+pkg install x11-toolkits/gtk30
+exit_code=$?
+if [ $exit_code != 0 ]; then
+    echo "error installing dependencies, there could be an error with your internet connection"
+    echo "if the error persists, please report an issue in github: http://github.com/openframeworks/openFrameworks/issues"
+	exit $exit_code
+fi
+
+
+echo "installing oF dependencies"
+echo "installing dependencies *not* ported yet"
+exit 23
+pkg install
+freeglut3-dev
+libasound2-dev
+libxmu-dev
+libxxf86vm-dev
+g++
+libgl1-mesa-dev
+libglu1-mesa-dev
+libraw1394-dev
+libudev-dev
+libdrm-dev
+libglew-dev
+libopenal-dev
+libsndfile-dev
+libfreeimage-dev
+libcairo2-dev
+python-lxml
+python-argparse
+libfreetype6-dev
+libssl-dev
+libpulse-dev
+libusb-1.0-0-dev
+libopencv-dev
+libegl1-mesa-dev
+libgles1-mesa-dev
+libgles2-mesa-dev
+exit_code=$?
+if [ $exit_code != 0 ]; then
+    echo "error installing dependencies, there could be an error with your internet connection"
+    echo "if the error persists, please report an issue in github: http://github.com/openframeworks/openFrameworks/issues"
+	exit $exit_code
+fi
+
