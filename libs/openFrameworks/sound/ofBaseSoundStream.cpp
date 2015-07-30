11dfbd06adde84106c04f1ee034495130814a11a
tpltnt <tpltnt.github@dropcut.net>
2015-07-30 15:16:13 +0200

    cleaned up includes of ofBaseSoundStream.cpp

--- libs/openFrameworks/sound/ofBaseSoundStream.cpp
+++ libs/openFrameworks/sound/ofBaseSoundStream.cpp
@@ -1,8 +1,6 @@
-
-#include "ofSoundStream.h"
 #include "ofBaseSoundStream.h"
-#include "ofSoundBuffer.h"
-#include "ofLog.h"
+#include <ostream>  // for endl
+#include "ofLog.h"  // for ofLog, ofLogNotice
 
 void ofBaseSoundStream::printDeviceList() const {
 	ofLogNotice("ofBaseSoundStream::printDeviceList") << std::endl << getDeviceList();
